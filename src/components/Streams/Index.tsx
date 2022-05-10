3f3b61fa8fd0e0501ebb89fc8120c22329d09200
Marcelo Chaves <Marcelochaves20000@gmail.com>
2022-05-10 00:34:52 -0300

    commit

--- src/components/Streams/Index.tsx
+++ src/components/Streams/Index.tsx
@@ -6,9 +6,17 @@ export default function Streams() {
       <strong>Lives</strong>
       <hr />
       <p>
-        Acompanhe nosso manager e time durante o processo de treinamento e em campeonatos!
+        Acompanhe nosso manager e time durante o processo de treinamento e em
+        campeonatos!
       </p>
-       <iframe title="aaaa" src="https://player.twitch.tv/?channel=omanager1&parent=https://stomp-academy.vercel.app" frameBorder="0" allowFullScreen={true} scrolling="no" height="600" width="1200"></iframe>
+      <iframe
+        src="https://player.twitch.tv/?channel=omanager1&parent=https://stomp-academy.vercel.app/Contato"
+        frameBorder="0"
+        allowFullScreen={true}
+        scrolling="no"
+        height="600"
+        width="1200"
+      ></iframe>
     </Container>
   );
 }
